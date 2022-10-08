45e706a1106fa7ba97e4037045af2cc755c3a14a
Mohammad Mustafijur Rahman <rahmanbond0@gmail.com>
2022-10-08 22:28:43 +0800

    added new

--- index.js
+++ index.js
@@ -7,8 +7,8 @@ const multify = numbers.map(x => x + 1);
 const arrry = [33, 50, 79, 78, 90, 101, 30];
 const divide = arrry.filter(n => n % 10);
 const comperisom = arrry.find(n => n % 10);
-console.log(divide);
-console.log(comperisom);
+// console.log(divide);
+// console.log(comperisom);
 
 // 4:
 // console.log(defined);
@@ -22,3 +22,34 @@ const student = {
     }
 }
 // console.log(student.city.two)
+
+// 5
+
+const users = {
+    id: 1,
+    name: 'Leana Graham',
+    username: 'breat',
+    email: 'Sincere@gmail.com',
+    address: {
+        street: 'kulas light',
+        suite: 'Apt . 556',
+        city: 'lost angels',
+        zipcode: '92998-3874',
+        geo: {
+            lat: '-6561656562',
+            lng: '763trwq436y3',
+        }
+    },
+    phone: '001-65656251656521',
+    website: 'tomsKrick.web',
+    company: {
+        name: 'Ramugfera-vcoirhg',
+        catchParse: 'Multi-Layered client-server nural.net',
+        bs: 'harness real-time e markets',
+    }
+}
+console.log(users.email);
+console.log(users.address)
+console.log(users.address.city)
+console.log(users.address.geo.lat);
+console.log(users.company.name)
